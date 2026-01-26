3b121bcb261f2b27b4a1c1d37b5bb1cd8eacf7ca
gabrielnm123 <gabrielnmatos96@gmail.com>
2026-01-26 08:47:50 -0300

    criando passo a passo dos capitulos em notebook, de forma mais direta

--- capitulo2/tab_powerbi.py
+++
@@ -1,8 +0,0 @@
-from tabela.tabela import TabelaPb
-
-# tabela = TabelaPb(1, r'\\wsl.localhost\Ubuntu\home\gabriel\wp\   estatistica_basica\Dados_EB.xlsx', sheet_name='Tabela 2.1', sheet_name='Tabela 2.1', header=[1])
-tabela = TabelaPb(1, './Dados_EB.xlsx', sheet_name='Tabela 2.1', header=[1])
-tabela_2_1 = tabela.tab
-tabela_2_2 = tabela.frequencia_proporcao('Grau de Instrução')
-tabela_2_4 = tabela.frequencia_variavel_continua('Salario (x Sal Min)', amplitude=4)
-pass
